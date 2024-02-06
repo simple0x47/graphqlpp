66ac77c3323f1046248d03b0b6292a60825f6a67
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2024-02-06 20:09:53 +0100

    r naming to Google's style guide

--- src/graphqlpp/result.h
+++ src/graphqlpp/result.h
@@ -9,20 +9,20 @@ namespace graphqlpp {
 /// failure by containing two possible values.
 /// Its inner value can only be accessed once since the value's ownership
 /// is moved to the caller.
-/// \tparam Ok Type of the value related to the success of the operation.
-/// \tparam Error Type of the value related to the failure of the operation.
-template <typename Ok, typename Error>
+/// \tparam OkType Type of the value related to the success of the operation.
+/// \tparam ErrorType Type of the value related to the failure of the operation.
+template <typename OkType, typename ErrorType>
 class Result {
  public:
   /// \brief Whether the contained value is related to a success or a failure.
   /// \return True if the contained value is ok, false otherwise.
-  [[nodiscard]] bool isOk() const { return is_ok_; }
+  [[nodiscard]] bool IsOk() const { return is_ok_; }
 
   /// \brief Extracts the <b>Ok</b> value from the result. Throws an exception
   /// if the result does not contain a successful value.
   /// \return The <b>Ok</b> value from the result.
-  [[nodiscard]] std::unique_ptr<Ok> unwrap() {
-    if (!this->isOk()) {
+  [[nodiscard]] std::unique_ptr<OkType> Unwrap() {
+    if (!this->IsOk()) {
       throw std::logic_error("Unwrapped 'Ok' when the value was an 'Error'.");
     }
 
@@ -32,8 +32,8 @@ class Result {
   /// \brief Extracts the <b>Error</b> value from the result. Throws an
   /// exception if the result does not contain a successful value.
   /// \return The <b>Error</b> value from the result.
-  [[nodiscard]] std::unique_ptr<Error> unwrapErr() {
-    if (this->isOk()) {
+  [[nodiscard]] std::unique_ptr<ErrorType> UnwrapErr() {
+    if (this->IsOk()) {
       throw std::logic_error("Unwrapped 'Error' when the value was an 'Ok'.");
     }
 
@@ -43,25 +43,26 @@ class Result {
   /// \brief Creates a successful result containing an <b>Ok</b> value.
   /// \param o Ok value which is moved into an <i>unique_ptr</i>.
   /// \return Result containing the <b>Ok</b> value.
-  static Result ok(Ok o) {
-    return Result(std::make_unique<Ok>(std::move(o)), std::unique_ptr<Error>(),
-                  true);
+  static Result Ok(OkType o) {
+    return Result(std::make_unique<OkType>(std::move(o)),
+                  std::unique_ptr<ErrorType>(), true);
   }
 
   /// \brief Creates a failed result containing an <b>Error</b> value.
   /// \param e Error value which is moved into an <i>unique_ptr</i>.
   /// \return Result containing the <b>Error</b> value.
-  static Result err(Error e) {
-    return Result(std::unique_ptr<Ok>(), std::make_unique<Error>(std::move(e)),
-                  false);
+  static Result Err(ErrorType e) {
+    return Result(std::unique_ptr<OkType>(),
+                  std::make_unique<ErrorType>(std::move(e)), false);
   }
 
  private:
-  std::unique_ptr<Ok> ok_;
-  std::unique_ptr<Error> error_;
+  std::unique_ptr<OkType> ok_;
+  std::unique_ptr<ErrorType> error_;
   bool is_ok_;
 
-  Result(std::unique_ptr<Ok> v, std::unique_ptr<Error> e, const bool is_ok) {
+  Result(std::unique_ptr<OkType> v, std::unique_ptr<ErrorType> e,
+         const bool is_ok) {
     ok_ = std::move(v);
     error_ = std::move(e);
     is_ok_ = is_ok;
--- test/graphqlpp/result_test.cpp
+++ test/graphqlpp/result_test.cpp
@@ -12,68 +12,68 @@ class NoDefaultConstructor {
 };
 
 TEST(ResultTest, IsOk_ReturnsTrueForOkValue) {
-  const Result<int, bool> r = Result<int, bool>::ok(120);
+  const Result<int, bool> r = Result<int, bool>::Ok(120);
 
-  ASSERT_TRUE(r.isOk());
+  ASSERT_TRUE(r.IsOk());
 }
 
 TEST(ResultTest, IsOk_ReturnsFalseForErrValue) {
-  const Result<int, bool> r = Result<int, bool>::err(false);
+  const Result<int, bool> r = Result<int, bool>::Err(false);
 
-  ASSERT_FALSE(r.isOk());
+  ASSERT_FALSE(r.IsOk());
 }
 
 TEST(ResultTest, Ok_PointerCompatible) {
   std::string example = "hey";
-  const Result<std::string*, bool> r = Result<std::string*, bool>::ok(&example);
+  const Result<std::string*, bool> r = Result<std::string*, bool>::Ok(&example);
 
-  ASSERT_TRUE(r.isOk());
+  ASSERT_TRUE(r.IsOk());
 }
 
 TEST(ResultTest, Err_PointerCompatible) {
-  const Result<std::string*, bool> r = Result<std::string*, bool>::err(false);
+  const Result<std::string*, bool> r = Result<std::string*, bool>::Err(false);
 
-  ASSERT_FALSE(r.isOk());
+  ASSERT_FALSE(r.IsOk());
 }
 
 TEST(ResultTest, Ok_NoDefaultConstructorWorks) {
   const Result<bool, NoDefaultConstructor> r =
-      Result<bool, NoDefaultConstructor>::ok(false);
+      Result<bool, NoDefaultConstructor>::Ok(false);
 
-  ASSERT_TRUE(r.isOk());
+  ASSERT_TRUE(r.IsOk());
 }
 
 TEST(ResultTest, Err_NoDefaultConstructorWorks) {
   const Result<NoDefaultConstructor, bool> r =
-      Result<NoDefaultConstructor, bool>::err(false);
+      Result<NoDefaultConstructor, bool>::Err(false);
 
-  ASSERT_FALSE(r.isOk());
+  ASSERT_FALSE(r.IsOk());
 }
 
 TEST(ResultTest, Unwrap_ReturnsUniquePtrWithOkValue) {
-  Result<std::string, bool> r = Result<std::string, bool>::ok("yes");
+  Result<std::string, bool> r = Result<std::string, bool>::Ok("yes");
 
-  const std::unique_ptr<std::string> ok = r.unwrap();
+  const std::unique_ptr<std::string> ok = r.Unwrap();
 
   ASSERT_EQ("yes", *ok);
 }
 
 TEST(ResultTest, Unwrap_ThrowsExceptionForErrorValue) {
-  Result<std::string, bool> r = Result<std::string, bool>::err(false);
+  Result<std::string, bool> r = Result<std::string, bool>::Err(false);
 
-  ASSERT_THROW({ auto v = r.unwrap(); }, std::logic_error);
+  ASSERT_THROW({ auto v = r.Unwrap(); }, std::logic_error);
 }
 
 TEST(ResultTest, UnwrapErr_ReturnsUniquePtrWithErrorValue) {
-  Result<std::string, bool> r = Result<std::string, bool>::err(true);
+  Result<std::string, bool> r = Result<std::string, bool>::Err(true);
 
-  const std::unique_ptr<bool> error = r.unwrapErr();
+  const std::unique_ptr<bool> error = r.UnwrapErr();
 
   ASSERT_TRUE(error);
 }
 
 TEST(ResultTest, UnwrapErr_ThrowsExceptionForOkValue) {
-  Result<std::string, bool> r = Result<std::string, bool>::ok("yes");
+  Result<std::string, bool> r = Result<std::string, bool>::Ok("yes");
 
-  ASSERT_THROW({ auto e = r.unwrapErr(); }, std::logic_error);
+  ASSERT_THROW({ auto e = r.UnwrapErr(); }, std::logic_error);
 }
